83f8f048417494d887fca4a7f2aa7cd797de46c1
reeshal <rreeshal@gmail.com>
2020-02-22 19:10:39 +0400

    Merged with Hevin

--- NoPoverty/NoPoverty/NoPoverty/Views/Homepage.xaml.cs
+++ NoPoverty/NoPoverty/NoPoverty/Views/Homepage.xaml.cs
@@ -7,14 +7,28 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
 namespace NoPoverty.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Homepage : ContentPage
+
+    public partial class Homepage : Xamarin.Forms.TabbedPage
     {
+        
         public Homepage()
         {
             InitializeComponent();
+            CurrentPage = Children[1]; //initialising the 2nd tab as homepage
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+        }
+        private void NavigationPage_SizeChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void NavigationPage_LayoutChanged(object sender, EventArgs e)
+        {
         }
     }
 }
---
+++ NoPoverty/NoPoverty/NoPoverty/Views/NewItem.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Media;
+
+namespace NoPoverty.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewItem : ContentPage
+    {
+        public NewItem()
+        {
+            InitializeComponent();
+        }
+        private void Tab1Clicked(object sender, EventArgs e)
+        {
+            stkTab1.IsVisible = true;
+            stkTab2.IsVisible = false;
+        }
+
+        private void Tab2Clicked(object sender, EventArgs e)
+        {
+            stkTab1.IsVisible = false;
+            stkTab2.IsVisible = true;
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        private async void Camera_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported && !CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Message", "Photo Capture and Pick not supported", "ok");
+                return;
+            }
+            else
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Images",
+                    Name = DateTime.Now + "_test.jpg"
+                });
+
+                if (file == null)
+                    return;
+                await DisplayAlert("File Path", file.Path, "Ok");
+
+                MyImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+        }
+    }
+}
